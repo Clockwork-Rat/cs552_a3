92385934fe1d42c3ef9d15679949461db0c92cbe
Isaiah Raspet <isaiahraspet@gmail.com>
2024-03-16 14:03:30 -0700

    print unsigned

--- distribution_sort_act3_raspet.c
+++ distribution_sort_act3_raspet.c
@@ -192,7 +192,7 @@ int main(int argc, char **argv) {
   double etime = MPI_Wtime();
 
   if (my_rank == 0) {
-    printf("Global Sum: %d\n", global_sum);
+    printf("Global Sum: %u\n", global_sum);
     printf("Total time: %f\n", etime - stime);
   }
 
