c54bd38f2cb95bd9b42892dcaf485a2fffb526b4
Isaiah Raspet <isaiahraspet@gmail.com>
2024-03-16 14:06:58 -0700

    create submission sh

---
+++ work_scripts/submit.sh
@@ -0,0 +1,24 @@
+
+sbatch act1/act1_1.sh
+sbatch act1/act1_2.sh
+sbatch act1/act1_4.sh
+sbatch act1/act1_8.sh
+sbatch act1/act1_12.sh
+sbatch act1/act1_16.sh
+sbatch act1/act1_20.sh
+
+sbatch act2/act2_1.sh
+sbatch act2/act2_2.sh
+sbatch act2/act2_4.sh
+sbatch act2/act2_8.sh
+sbatch act2/act2_12.sh
+sbatch act2/act2_16.sh
+sbatch act2/act2_20.sh
+
+sbatch act3/act3_1.sh
+sbatch act3/act3_2.sh
+sbatch act3/act3_4.sh
+sbatch act3/act3_8.sh
+sbatch act3/act3_12.sh
+sbatch act3/act3_16.sh
+sbatch act3/act3_20.sh
